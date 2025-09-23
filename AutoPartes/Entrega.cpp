c3c2f5519c109e2b15f91dd49fcb339994b18e6c
Acosta R. Agustín <76490549+jxagus@users.noreply.github.com>
2025-09-23 17:34:33 -0300

    entrega cpp + .h

---
+++ AutoPartes/Entrega.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+     return 0;
+}
+#include "Entrega.h"
+
+Entrega::Entrega()
+{
+    //ctor
+}
+
+Entrega::~Entrega()
+{
+    //dtor
+}
---
+++ AutoPartes/Entrega.h
@@ -0,0 +1,16 @@
+#ifndef ENTREGA_H
+#define ENTREGA_H
+
+
+class Entrega
+{
+    public:
+        Entrega();
+        virtual ~Entrega();
+
+    protected:
+
+    private:
+};
+
+#endif // ENTREGA_H
