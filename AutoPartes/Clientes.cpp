87cd94fead90d979f422751084a8b4adc6b59968
Acosta R. Agustín <76490549+jxagus@users.noreply.github.com>
2025-09-23 17:33:18 -0300

    Clase clientes

---
+++ AutoPartes/Clientes.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+     return 0;
+}
+#include "Clientes.h"
+
+Clientes::Clientes()
+{
+    //ctor
+}
+
+Clientes::~Clientes()
+{
+    //dtor
+}
