4d5eb72e84b519da4310caef27b4afc6a671d745
Acosta R. Agustín <76490549+jxagus@users.noreply.github.com>
2025-10-17 16:03:33 -0300

    venta archivo.h

--- AutoPartes/VentaArchivo.h
+++ AutoPartes/VentaArchivo.h
@@ -1,16 +1,19 @@
-#ifndef VENTAARCHIVO_H
-#define VENTAARCHIVO_H
+#ifndef VENTAARCHIVO_H_INCLUDED
+#define VENTAARCHIVO_H_INCLUDED
+#include "Venta.h"
 
+class VentaArchivo {
+private:
+    const char* _nombreArchivo;
+public:
+    VentaArchivo(const char* nombreArchivo = "ventas.dat");
 
-class VentaArchivo
-{
-    public:
-        VentaArchivo();
-        virtual ~VentaArchivo();
-
-    protected:
-
-    private:
+    bool guardar(const Venta& reg);
+    Venta leer(int pos);
+    int getCantidadRegistros();
+    int buscarPorID(int idVenta);
+    bool modificar(const Venta& reg, int pos);
+    bool leerTodos(Venta* vec, int cantidad);
 };
 
-#endif // VENTAARCHIVO_H
+#endif // VENTAARCHIVO_H_INCLUDED
